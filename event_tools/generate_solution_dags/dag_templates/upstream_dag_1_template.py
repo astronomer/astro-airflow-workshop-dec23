959d0231788feefe107dec08c7977d0fe6264c46
TJaniF <tamara.fingerlin@astronomer.io>
2024-08-26 17:52:26 +0200

    LazyXComSelectSequence

--- event_tools/generate_solution_dags/dag_templates/upstream_dag_1_template.py
+++ event_tools/generate_solution_dags/dag_templates/upstream_dag_1_template.py
@@ -180,7 +180,7 @@ def UPSTREAM_DAG_ID_1_TO_REPLACE():
             weather: The weather data for the cities of interest, in JSON format.
             cities_coordinates: The coordinates of the cities of interest.
         """
-        from airflow.models.xcom import LazyXComAccess
+        from airflow.models.xcom import LazyXComSelectSequence
         import json
         from tabulate import tabulate
         from include.helper_functions import (
@@ -197,7 +197,7 @@ def UPSTREAM_DAG_ID_1_TO_REPLACE():
         weather = weather if isinstance(weather, list) else [weather]
         cities_coordinates = (
             list(cities_coordinates)
-            if isinstance(cities_coordinates, LazyXComAccess)
+            if isinstance(cities_coordinates, LazyXComSelectSequence)
             else [cities_coordinates]
         )
 
